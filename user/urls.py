6b9c2dc784a13eacbe2aeefa9bfdd58aa67ac7bd
yaswanth <yaswanth.s@gndsolutions.in>
2025-05-19 11:30:22 +0530

    Added reset password view

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import login_view, register_user, send_otp_view
+from .views import login_view, register_user, reset_password_view, send_otp_view
 
 
 
 urlpatterns = [
     path('register/',register_user,name= 'register'),
     path('login/', login_view, name='login'),
-    path('send-otp/',send_otp_view,name='send-otp')
+    path('send-otp/',send_otp_view,name='send-otp'),
+    path('reset-password',reset_password_view,name = 'reset-password'),
 ]
